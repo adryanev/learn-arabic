064652ff0517a835bdb8ba80488992d727aa8bfd
adryanev <adryanekavandra@gmail.com>
2017-11-06 09:54:03 +0700

    add error page

---
+++ views/site/error.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adryanev
+ * Date: 06/11/17
+ * Time: 8:49
+ */
+use yii\helpers\Html;
+
+$this->title = 'Beranda';
+?>
+
+<div class="card">
+
+    <div class="card-header"  data-background-color="red">
+        <h3 class="title">ERROR</h3>
+
+    </div>
+    <div class="card-content">
+        <p> Maaf, Halaman yang anda cari tidak ada.</p>
+    </div>
+</div>
